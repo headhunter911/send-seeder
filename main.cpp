9d7afe1b7b95a9c04d2faadad05cc012d7795325
headhunter911 <35839355+headhunter911@users.noreply.github.com>
2018-02-03 18:55:04 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -162,7 +162,7 @@ extern "C" void* ThreadCrawler(void* data) {
       res.nClientV = 0;
       res.nHeight = 0;
       res.strClientV = "";
-      bool = res.ourLastSuccess + 1000 < now;
+      bool getaddr = res.ourLastSuccess + 1000 < now;
       res.fGood = TestNode(res.service,res.nBanTime,res.nClientV,res.strClientV,res.nHeight,getaddr ? &addr : NULL);
     }
     db.ResultMany(ips);
